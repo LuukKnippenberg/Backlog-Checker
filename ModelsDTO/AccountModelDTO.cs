f1c3d514546f396be4188e0d0a9b460994d967f5
Luuk Knippenberg <luuk_knippenberg@hotmail.com>
2020-12-08 19:14:45 +0100

    Created Account class and AccountModelDTO
    - Created Account Class
    - Renamed AccountsModel into AccountModelDTO

---
+++ ModelsDTO/AccountModelDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelsDTO
+{
+    public class AccountModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string AvatarUrl { get; set; }
+        public string HeaderUrl { get; set; }
+        public string Rights { get; set; }
+
+    }
+}
